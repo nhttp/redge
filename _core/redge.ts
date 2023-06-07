cfde7f112cd88f9342b7c30b8a659577da34eff3
herudi <herudi7@gmail.com>
2023-06-07 11:13:43 +0700

    esbuild

--- _core/redge.ts
+++ _core/redge.ts
@@ -53,21 +53,21 @@ class Esbuild {
     jsxImportSource: "react",
   };
 
-  async getEsbuild() {
+  static async initWasm() {
     if (isDeploy) {
       const wasmModule = await WebAssembly.compileStreaming(
         fetch("https://unpkg.com/esbuild-wasm@0.17.19/esbuild.wasm"),
       );
       await esbuildWasm.initialize({ wasmModule, worker: false });
-      return esbuildWasm;
     }
-    return esbuildOri;
+  }
+
+  get esbuild() {
+    return isDeploy ? esbuildWasm : esbuildOri;
   }
 }
 
-const es = new Esbuild();
-const config = es.config;
-const esbuild = await es.getEsbuild();
+await Esbuild.initWasm();
 
 function isEmptyObj(props: TAny) {
   if (!props) return false;
@@ -86,16 +86,17 @@ const setHeader = (rev: RequestEvent) => {
 const delay = (t: number) => new Promise((ok) => setTimeout(ok, t));
 export class Redge extends NHttp {
   #entry: Record<string, TAny> = {};
-  #cache = new Map();
+  #cache: Record<string, Uint8Array> = {};
+  #es = new Esbuild();
   #awaiter = async (path: string) => {
     await delay(1000);
     let i = 0;
-    while (!this.#cache.has(path)) {
+    while (this.#cache[path] === void 0) {
       await delay(1000);
-      if (this.#cache.has(path) || i === 10) break;
+      if (this.#cache[path] !== void 0 || i === 10) break;
       i++;
     }
-    return this.#cache.get(path) ?? new Response(null, { status: 204 });
+    return this.#cache[path] ?? new Response(null, { status: 204 });
   };
   constructor(opts: TApp = {}) {
     super(opts);
@@ -159,7 +160,7 @@ export class Redge extends NHttp {
       }
       const path = `${fn.path}.js`;
       src.push(`<script type="module" src="${path}" async></script>`);
-      if (!this.#cache.has(path)) {
+      if (this.#cache[path] === void 0) {
         const key = fn.path.substring(1);
         this.#entry[key] = fn.meta_url;
       }
@@ -177,43 +178,49 @@ export class Redge extends NHttp {
       client: "redge/client",
       react: "react",
     };
-    return esbuild.build({
-      ...config,
+    return this.#es.esbuild.build({
+      ...this.#es.config,
       entryPoints,
       write: false,
     });
   };
   #createAssets = () => {
-    if (!isEmptyObj(this.#entry)) {
-      const awaiter = this.#awaiter.bind(this);
-      for (const k in this.#entry) {
+    // deno-lint-ignore no-this-alias
+    const self = this;
+    if (!isEmptyObj(self.#entry)) {
+      const awaiter = self.#awaiter.bind(this);
+      for (const k in self.#entry) {
         const path = "/" + k + ".js";
-        this.get(path, async (rev) => {
+        self.get(path, async (rev) => {
           setHeader(rev);
-          return this.#cache.get(path) ?? await awaiter(path);
+          return self.#cache[path] ?? await awaiter(path);
         });
       }
-      this.get(`/redge.${tt}.js`, async (rev) => {
-        if (!isEmptyObj(this.#entry)) {
+      self.get(`/redge.${tt}.js`, async (rev) => {
+        if (!isEmptyObj(self.#entry)) {
           try {
-            const res = await this.#bundle();
+            console.log(self.#entry);
+            const res = await self.#bundle();
             const files = res.outputFiles;
             files.forEach(({ path, contents }) => {
               path = toPathname(path);
-              if (!this.#cache.has(path)) {
-                this.#cache.set(path, contents);
-                this.get(path, (rev) => {
+              if (self.#cache[path] === void 0) {
+                self.#cache[path] = contents;
+                self.get(path, (rev) => {
                   setHeader(rev);
                   return contents;
                 });
               }
             });
-            if (!isDeploy) esbuild.stop();
-            this.#entry = {};
-          } catch {
-            for (const k in this.#entry) {
+            if (!isDeploy) this.#es.esbuild.stop();
+            self.#entry = {};
+          } catch (err) {
+            console.error(err);
+            for (const k in self.#entry) {
               const path = "/" + k + ".js";
-              this.#cache.set(path, "to many reload !!");
+              self.#cache[path] = new Uint8Array(
+                new TextEncoder().encode("NOOP"),
+              );
             }
             setHeader(rev);
             return "location.reload();";
